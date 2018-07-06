eed63ceee8b695401e4e2e222385449b8ececb12
kingdido999 <kingdido999@gmail.com>
2018-07-06 15:59:57 -0700

    Add apollo link state

---
+++ src/apollo/client.js
@@ -0,0 +1,23 @@
+import { ApolloClient } from 'apollo-client'
+import { ApolloLink } from 'apollo-link'
+import { HttpLink } from 'apollo-link-http'
+import { withClientState } from 'apollo-link-state'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const cache = new InMemoryCache()
+
+const httpLink = new HttpLink({ uri: 'http://localhost:3001/graphql' })
+const stateLink = withClientState({
+  cache
+})
+
+const client = new ApolloClient({
+  // By default, this client will send queries to the
+  //  `/graphql` endpoint on the same host
+  // Pass the configuration option { uri: YOUR_GRAPHQL_API_URL } to the `HttpLink` to connect
+  // to a different host
+  link: ApolloLink.from([stateLink, httpLink]),
+  cache: new InMemoryCache()
+})
+
+export default client
--- src/index.js
+++ src/index.js
@@ -2,22 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo'
 import { ThemeProvider } from 'styled-components'
 import theme from './theme'
 import { BrowserRouter } from 'react-router-dom'
-
-const client = new ApolloClient({
-  // By default, this client will send queries to the
-  //  `/graphql` endpoint on the same host
-  // Pass the configuration option { uri: YOUR_GRAPHQL_API_URL } to the `HttpLink` to connect
-  // to a different host
-  link: new HttpLink({ uri: 'http://localhost:3001/graphql' }),
-  cache: new InMemoryCache()
-})
+import client from './apollo/client'
 
 ReactDOM.render(
   <ApolloProvider client={client}>
