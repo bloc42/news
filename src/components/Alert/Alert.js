0cc59061393f30bd47ae9922f22dea289f141ef9
kingdido999 <kingdido999@gmail.com>
2018-07-09 17:56:15 -0700

    Add Alert component

---
+++ src/components/Alert/Alert.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled, { css } from 'styled-components'
+
+const StyledAlert = styled.div`
+  margin: 0.4rem 0;
+  padding: 1rem;
+  border-radius: ${props => props.theme.borderRadius};
+  color: ${props => props.theme.fontColorLight};
+
+  /* Error Alert */
+  ${props =>
+    props.error &&
+    css`
+      border: 1px solid #ffa39e;
+      background-color: #fff1f0;
+    `};
+`
+
+const Alert = ({ message }) => <StyledAlert>{message}</StyledAlert>
+
+Alert.propTypes = {
+  message: PropTypes.string.isRequired
+}
+
+export default Alert
