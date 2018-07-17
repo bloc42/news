e7a61d70cfbedeab89edb853bf19ec021b53531d
kingdido999 <kingdido999@gmail.com>
2018-07-17 12:34:34 -0700

    Fix page component names

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import HomePage from '../../pages/HomePage'
-import Login from '../../pages/Login'
-import Signup from '../../pages/Signup'
-import SubmitPost from '../../pages/SubmitPost'
+import LoginPage from '../../pages/LoginPage'
+import SignupPage from '../../pages/SignupPage'
+import SubmitPostPage from '../../pages/SubmitPostPage'
 import styled from 'styled-components'
 import PostPage from '../../pages/PostPage'
-import UserProfile from '../../pages/UserProfile'
+import UserProfilePage from '../../pages/UserProfilePage'
 
 const StyledMain = styled.main`
   flex: 1;
@@ -18,11 +18,11 @@ const Main = () => (
   <StyledMain>
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={Signup} />
-      <Route path="/submit" component={SubmitPost} />
+      <Route path="/login" component={LoginPage} />
+      <Route path="/signup" component={SignupPage} />
+      <Route path="/submit" component={SubmitPostPage} />
       <Route path="/post/:id" component={PostPage} />
-      <Route path="/user/:username" component={UserProfile} />
+      <Route path="/user/:username" component={UserProfilePage} />
     </Switch>
   </StyledMain>
 )
