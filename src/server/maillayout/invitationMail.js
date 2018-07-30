4162ebff05b4d89f6737a43da1b0884ab1d68a72
kingdido999 <kingdido999@gmail.com>
2018-07-30 13:23:21 -0700

    Merge develop into feat/invite-code

--- src/server/maillayout/invitationMail.js
+++ src/server/maillayout/invitationMail.js
@@ -1,4 +1,4 @@
-const invitationMail = (sender,link) => {
+const invitationMail = (sender, link) => {
   const template = `<div><h2>Hello!我们是Block42</h2><p>${sender}邀请您加入Block42</p><p><a href=${link}>立即加入</a></p><p>如果点击没有反应,请将以下连接复制到浏览器</p><p>${link}</p></div>`
   return template
 }
