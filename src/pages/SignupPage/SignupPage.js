842f3451cff018849b21e892181abbad1044707e
hacksummer <zhouyin@great-tao.com>
2018-11-06 11:18:40 +0800

    rewrite new signup page

--- src/pages/SignupPage/SignupPage.js
+++ src/pages/SignupPage/SignupPage.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import Section from '../../components/Section'
-import SignupForm from '../../containers/SignupForm'
 import Container from '../../components/Container'
-import Small from '../../components/Small'
 import Link from '../../components/Link'
+import Form from '../../components/Form'
+import Button from '../../components/Button'
+import Small from '../../components/Small'
 
-const SignupPage = () => {
+const SignupPage = props => {
   return (
     <Container small>
       <Section padded>
         <h2>创建属于你的唠嗑账号。</h2>
-        <SignupForm />
+        <Form>
+          <Form.Item>
+            <Link to="/creatNewEtherAccount">
+              <Button primary fullWidth>
+                未拥有以太坊账户
+              </Button>
+            </Link>
+          </Form.Item>
+          <Form.Item>
+            <Link to="/importEtherAccount">
+              <Button primary fullWidth>
+                我已拥有以太坊账户
+              </Button>
+            </Link>
+          </Form.Item>
+        </Form>
         <Small>
           <span>已经有账号？</span>
           <Link to="/login">登录</Link>
