306bb20c40d51a651047140b71fc1ce224122545
hacksummer <zhouyin@great-tao.com>
2018-12-14 11:55:40 +0800

    fixbug:删除帖子，弹出确认框，确认后回到首页

--- src/pages/PostPage/PostPage.js
+++ src/pages/PostPage/PostPage.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Query } from 'react-apollo'
+import { graphql, compose } from 'react-apollo'
 import gql from 'graphql-tag'
 import Container from '../../components/Container'
 import Section from '../../components/Section'
@@ -11,6 +12,10 @@ import styled from 'styled-components'
 import Comment from '../../components/Comment'
 import Divider from '../../components/Divider'
 import ReactMarkdown from 'react-markdown'
+import Modal from '../../components/Modal'
+import { GET_POSTS } from '../../containers/PostList'
+import { withRouter } from 'react-router-dom'
+import _ from 'lodash'
 
 const StyledArticle = styled.article`
   > p {
@@ -62,181 +67,252 @@ export const GET_POST = gql`
     }
   }
 `
+class PostPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showModal: false,
+      tips: ``
+    }
+  }
+  showModal = e => {
+    this.setState({ showModal: true })
+  }
+  closeModal = e => {
+    this.setState({ showModal: false })
+  }
+  delPost = e => {
+    this.setState({ tips: `确认删除此贴?` })
+    this.showModal()
+  }
+  confirmCallback = async e => {
+    const { id } = this.props.match.params
+    try {
+      const errors = await this.props
+        .delPostMutation({
+          variables: {
+            id
+          },
+          update: (cache, { data }) => {
+            if (data.delPost == true) {
+              const { postFeed } = cache.readQuery({ query: GET_POSTS })
+              const delId = id
+              const { posts } = postFeed
 
-const PostPage = props => {
-  const { id } = props.match.params
+              const index = posts.findIndex(post => post.id === delId)
+              if (index > -1) {
+                posts.splice(index, 1)
+              }
+              cache.writeQuery({
+                query: GET_POSTS,
+                data: {
+                  postFeed: {
+                    ...postFeed,
+                    posts: posts
+                  }
+                }
+              })
+            }
+          }
+        })
+        .then(res => {
+          this.closeModal()
+          this.props.history.push('/')
+        })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  render() {
+    const { id } = this.props.match.params
+    return (
+      <Container>
+        <Section padded>
+          <Query query={GET_POST} variables={{ id }}>
+            {({ loading, data }) => {
+              if (loading) return '加载中...'
 
-  return (
-    <Container>
-      <Section padded>
-        <Query query={GET_POST} variables={{ id }}>
-          {({ loading, data }) => {
-            if (loading) return '加载中...'
+              const {
+                title,
+                url,
+                content,
+                author,
+                channel,
+                createdAt,
+                commentCount,
+                comments
+              } = data.postById
 
-            const {
-              title,
-              url,
-              content,
-              author,
-              channel,
-              createdAt,
-              commentCount,
-              comments
-            } = data.postById
+              const { currentUser } = data
 
-            const { currentUser } = data
+              const postTitle = url ? (
+                <Anchor href={url} target="_blank">
+                  {title}
+                </Anchor>
+              ) : (
+                title
+              )
 
-            const postTitle = url ? (
-              <Anchor href={url} target="_blank">
-                {title}
-              </Anchor>
-            ) : (
-              title
-            )
-
-            const postUrl = url ? (
-              <p>
-                <a href={url} target="_blank">
-                  {url}
-                </a>
-              </p>
-            ) : (
-              ''
-            )
-
-            const postEdit =
-              currentUser && currentUser.username == author ? (
-                <Styledflexli>
-                  <span>
-                    <Divider />
-                  </span>
-                  <span>
-                    <Anchor href={`/edit/${id}`}>编辑</Anchor>
-                  </span>
-                </Styledflexli>
+              const postUrl = url ? (
+                <p>
+                  <a href={url} target="_blank">
+                    {url}
+                  </a>
+                </p>
               ) : (
                 ''
               )
 
-            const postDel =
-              currentUser && currentUser.username == author ? (
-                <Styledflexli>
-                  <span>
-                    <Divider />
-                  </span>
-                  <span>
-                    <Anchor href={`/del/${id}`}>删除</Anchor>
-                  </span>
-                </Styledflexli>
-              ) : (
-                ''
-              )
+              const postEdit =
+                currentUser && currentUser.username == author ? (
+                  <Styledflexli>
+                    <span>
+                      <Divider />
+                    </span>
+                    <span>
+                      <Anchor href={`/edit/${id}`}>编辑</Anchor>
+                    </span>
+                  </Styledflexli>
+                ) : (
+                  ''
+                )
 
-            const creatorDel = channel ? (
-              <Query
-                query={gql`
-                  query GetChannelInfo($channel: String!) {
-                    channel(name: $channel) {
-                      id
-                      name
-                      info
-                      logo
-                      creator
+              const postDel =
+                currentUser && currentUser.username == author ? (
+                  <Styledflexli>
+                    <span>
+                      <Divider />
+                    </span>
+                    <span>
+                      <Anchor onClick={this.delPost}>删除</Anchor>
+                    </span>
+                  </Styledflexli>
+                ) : (
+                  ''
+                )
+
+              const creatorDel = channel ? (
+                <Query
+                  query={gql`
+                    query GetChannelInfo($channel: String!) {
+                      channel(name: $channel) {
+                        id
+                        name
+                        info
+                        logo
+                        creator
+                      }
                     }
-                  }
-                `}
-                variables={{ channel }}
-              >
-                {({ loading, data }) => {
-                  if (loading) {
-                    return null
-                  }
-                  const { channel } = data
-                  const { creator } = channel
+                  `}
+                  variables={{ channel }}
+                >
+                  {({ loading, data }) => {
+                    if (loading) {
+                      return null
+                    }
+                    const { channel } = data
+                    const { creator } = channel
 
-                  if (
-                    currentUser &&
-                    creator == currentUser.username &&
-                    creator !== author
-                  ) {
-                    return (
-                      <Styledflexli>
-                        <span>
+                    if (
+                      currentUser &&
+                      creator == currentUser.username &&
+                      creator !== author
+                    ) {
+                      return (
+                        <Styledflexli>
+                          <span>
+                            <Divider />
+                          </span>
+                          <span>
+                            <Anchor onClick={this.delPost}>删除</Anchor>
+                          </span>
+                        </Styledflexli>
+                      )
+                    } else {
+                      return ''
+                    }
+                  }}
+                </Query>
+              ) : (
+                ''
+              )
+
+              return (
+                <div>
+                  <StyledArticle>
+                    <h2>{postTitle}</h2>
+                    {postUrl}
+                    <section>
+                      <ReactMarkdown source={content} />
+                    </section>
+                    <footer>
+                      <ul>
+                        <li>
+                          <Link to={`/user/${author}`}>{author}</Link>
+                        </li>
+                        <li>
                           <Divider />
-                        </span>
-                        <span>
-                          <Anchor href={`/del/${id}`}>删除</Anchor>
-                        </span>
-                      </Styledflexli>
-                    )
-                  } else {
-                    return ''
-                  }
-                }}
-              </Query>
-            ) : (
-              ''
-            )
+                        </li>
+                        <li>
+                          <RelativeTime timestamp={createdAt} />
+                        </li>
+                        <li>
+                          <Divider />
+                        </li>
+                        <li>
+                          <Link
+                            to={`/post/${id}`}
+                          >{`${commentCount}条评论`}</Link>
+                        </li>
 
-            return (
-              <div>
-                <StyledArticle>
-                  <h2>{postTitle}</h2>
-                  {postUrl}
+                        {postEdit}
+                        {postDel}
+                        {creatorDel}
+                      </ul>
+                    </footer>
+                  </StyledArticle>
                   <section>
-                    <ReactMarkdown source={content} />
+                    <SubmitCommentForm
+                      toggleReply={() => {}}
+                      postId={id}
+                      parentAuthor={author}
+                    />
+
+                    {comments.map(comment => {
+                      return (
+                        <Comment
+                          key={comment.id}
+                          {...comment}
+                          currentUser={currentUser}
+                        />
+                      )
+                    })}
                   </section>
-
-                  <footer>
-                    <ul>
-                      <li>
-                        <Link to={`/user/${author}`}>{author}</Link>
-                      </li>
-                      <li>
-                        <Divider />
-                      </li>
-                      <li>
-                        <RelativeTime timestamp={createdAt} />
-                      </li>
-                      <li>
-                        <Divider />
-                      </li>
-                      <li>
-                        <Link
-                          to={`/post/${id}`}
-                        >{`${commentCount}条评论`}</Link>
-                      </li>
-
-                      {postEdit}
-                      {postDel}
-                      {creatorDel}
-                    </ul>
-                  </footer>
-                </StyledArticle>
-                <section>
-                  <SubmitCommentForm
-                    toggleReply={() => {}}
-                    postId={id}
-                    parentAuthor={author}
-                  />
-
-                  {comments.map(comment => {
-                    return (
-                      <Comment
-                        key={comment.id}
-                        {...comment}
-                        currentUser={currentUser}
-                      />
-                    )
-                  })}
-                </section>
-              </div>
-            )
-          }}
-        </Query>
-      </Section>
-    </Container>
-  )
+                </div>
+              )
+            }}
+          </Query>
+          {this.state.showModal ? (
+            <Modal
+              tips={this.state.tips}
+              confirmCallback={this.confirmCallback}
+              closeModal={this.closeModal}
+            />
+          ) : (
+            ''
+          )}
+        </Section>
+      </Container>
+    )
+  }
 }
+const DEL_POST_MUTATION = gql`
+  mutation DelPost($id: ID!) {
+    delPost(id: $id)
+  }
+`
 
-export default PostPage
+const PostPageWithMutation = compose(
+  graphql(DEL_POST_MUTATION, { name: 'delPostMutation' })
+)(PostPage)
+export default withRouter(PostPageWithMutation)
